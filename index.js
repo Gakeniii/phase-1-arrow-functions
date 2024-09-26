54b23277dc829f3a6b1c5f54c45527554955b896
Sifa Gakeni Muriithi <gakenisifa@gmail.com>
2024-09-26 19:07:42 +0300

    Arrow functions

---
+++ index.js
@@ -0,0 +1,11 @@
+// DIVISION WITH ARROW FUNCTIONS
+const divide = (a,b) => a / b;
+divide(2000,100);
+
+// MULTIPLICATION WITH ARROW FUNCTIONS
+const square = x => x ** 2;
+square(2);
+ 
+// ADDITION 
+const add = (a,b) => a + b;
+add(3,4);
